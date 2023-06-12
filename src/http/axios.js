0dcb21664cfa7532a01d9222269d54df7eecc5fe
xiacan <1250891041@qq.com>
2023-06-12 08:40:58 +0800

    axios

--- src/http/axios.js
+++ src/http/axios.js
@@ -4,7 +4,7 @@ import {showLoading,hideLoading } from '@/utils/eloading'
 
 //创建axios实例
 const instance=axios.create({
-    baseURL:'http://api.jqrjq.cn/',
+    baseURL:'https://api.jqrjq.cn/',
     timeout:6000
 })
 //请求拦截器
